0de5c99c585574935b93bd1660f7f4dd6106dead
piriwata <piriwata@gmail.com>
2023-05-26 16:34:18 +0900

    Setup Storybook

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/nextjs"
+import * as path from "node:path"
+
+const config: StorybookConfig = {
+  stories: ["stories/*.stories.@(ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/nextjs",
+  docs: {
+    autodocs: false,
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/lib': path.resolve(__dirname, '../lib'),
+      '@/components': path.resolve(__dirname, '../components'),
+    }
+    return config
+  }
+}
+export default config
